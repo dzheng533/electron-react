42a7c8bcfa134ddbc69cb2d13b09a65119baba09
vincent <dzheng533@gmail.com>
2019-11-01 18:38:46 +0800

    提交

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
+  let preload = path.join(__dirname, 'preload.js');
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -20,7 +21,7 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
--- preload.js
+++ preload.js
@@ -1,5 +1,8 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+window.ipcRenderer = require('electron').ipcRenderer;
+console.log("Preload exec.")
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -9,4 +12,5 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+  console.log(window.ipcRenderer);  
 })
